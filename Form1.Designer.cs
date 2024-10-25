2dc8690d9473f46db2bf28dd9b3f16999319ce7c
Raine DeVries <362571390@GoTVDSB.ca>
2024-10-25 09:17:12 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Topic_1_assingment_GUI_Part_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.btnDivid = new System.Windows.Forms.Button();
+            this.btnMul = new System.Windows.Forms.Button();
+            this.lblPlus = new System.Windows.Forms.Label();
+            this.lblAdd = new System.Windows.Forms.Label();
+            this.lblSub = new System.Windows.Forms.Label();
+            this.lblMulti = new System.Windows.Forms.Label();
+            this.lblDivid = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlus.Location = new System.Drawing.Point(75, 47);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(85, 67);
+            this.btnPlus.TabIndex = 0;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMinus.Location = new System.Drawing.Point(75, 120);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(85, 68);
+            this.btnMinus.TabIndex = 1;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = true;
+            this.btnMinus.Click += new System.EventHandler(this.btnMinus_Click);
+            // 
+            // btnDivid
+            // 
+            this.btnDivid.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDivid.Location = new System.Drawing.Point(75, 258);
+            this.btnDivid.Name = "btnDivid";
+            this.btnDivid.Size = new System.Drawing.Size(85, 70);
+            this.btnDivid.TabIndex = 2;
+            this.btnDivid.Text = "/";
+            this.btnDivid.UseVisualStyleBackColor = true;
+            this.btnDivid.Click += new System.EventHandler(this.btnDivid_Click);
+            // 
+            // btnMul
+            // 
+            this.btnMul.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMul.Location = new System.Drawing.Point(75, 194);
+            this.btnMul.Name = "btnMul";
+            this.btnMul.Size = new System.Drawing.Size(85, 58);
+            this.btnMul.TabIndex = 3;
+            this.btnMul.Text = "*";
+            this.btnMul.UseVisualStyleBackColor = true;
+            this.btnMul.Click += new System.EventHandler(this.btnMul_Click);
+            // 
+            // lblPlus
+            // 
+            this.lblPlus.AutoSize = true;
+            this.lblPlus.Location = new System.Drawing.Point(178, 47);
+            this.lblPlus.Name = "lblPlus";
+            this.lblPlus.Size = new System.Drawing.Size(0, 13);
+            this.lblPlus.TabIndex = 4;
+            // 
+            // lblAdd
+            // 
+            this.lblAdd.AutoSize = true;
+            this.lblAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAdd.Location = new System.Drawing.Point(184, 62);
+            this.lblAdd.Name = "lblAdd";
+            this.lblAdd.Size = new System.Drawing.Size(196, 42);
+            this.lblAdd.TabIndex = 5;
+            this.lblAdd.Text = "8 + 8 = 16\r\n";
+            this.lblAdd.Click += new System.EventHandler(this.lblAdd_Click);
+            // 
+            // lblSub
+            // 
+            this.lblSub.AutoSize = true;
+            this.lblSub.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSub.Location = new System.Drawing.Point(184, 136);
+            this.lblSub.Name = "lblSub";
+            this.lblSub.Size = new System.Drawing.Size(164, 42);
+            this.lblSub.TabIndex = 6;
+            this.lblSub.Text = "8 - 8 = 0";
+            this.lblSub.Click += new System.EventHandler(this.lblSub_Click);
+            // 
+            // lblMulti
+            // 
+            this.lblMulti.AutoSize = true;
+            this.lblMulti.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMulti.Location = new System.Drawing.Point(184, 205);
+            this.lblMulti.Name = "lblMulti";
+            this.lblMulti.Size = new System.Drawing.Size(188, 42);
+            this.lblMulti.TabIndex = 7;
+            this.lblMulti.Text = "8 * 8 = 64";
+            // 
+            // lblDivid
+            // 
+            this.lblDivid.AutoSize = true;
+            this.lblDivid.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDivid.Location = new System.Drawing.Point(186, 275);
+            this.lblDivid.Name = "lblDivid";
+            this.lblDivid.Size = new System.Drawing.Size(162, 42);
+            this.lblDivid.TabIndex = 8;
+            this.lblDivid.Text = "8 / 8 = 0";
+            this.lblDivid.Click += new System.EventHandler(this.lblDivid_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblDivid);
+            this.Controls.Add(this.lblMulti);
+            this.Controls.Add(this.lblSub);
+            this.Controls.Add(this.lblAdd);
+            this.Controls.Add(this.lblPlus);
+            this.Controls.Add(this.btnMul);
+            this.Controls.Add(this.btnDivid);
+            this.Controls.Add(this.btnMinus);
+            this.Controls.Add(this.btnPlus);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Button btnMinus;
+        private System.Windows.Forms.Button btnDivid;
+        private System.Windows.Forms.Button btnMul;
+        private System.Windows.Forms.Label lblPlus;
+        private System.Windows.Forms.Label lblAdd;
+        private System.Windows.Forms.Label lblSub;
+        private System.Windows.Forms.Label lblMulti;
+        private System.Windows.Forms.Label lblDivid;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Topic_1_assingment_GUI_Part_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lblDivid.Visible = false;   
+            lblAdd.Visible = false;
+            lblMulti.Visible = false;
+            lblSub.Visible = false;
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+
+            lblDivid.Visible = false;
+            lblAdd.Visible = false;
+            lblMulti.Visible = false;
+            lblSub.Visible = false; lblAdd.Visible = true;
+        }
+
+        private void btnDivid_Click(object sender, EventArgs e)
+        {
+
+            lblDivid.Visible = false;
+            lblAdd.Visible = false;
+            lblMulti.Visible = false;
+            lblSub.Visible = false; lblDivid.Visible=true;
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblSub_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblDivid_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            lblDivid.Visible = false;
+            lblAdd.Visible = false;
+            lblMulti.Visible = false;
+            lblSub.Visible = false;
+            lblSub.Visible = true;
+        }
+
+        private void btnMul_Click(object sender, EventArgs e)
+        {
+            lblDivid.Visible = false;
+            lblAdd.Visible = false;
+            lblMulti.Visible = false;
+            lblSub.Visible = false;
+            lblMulti.Visible = true;
+        }
+    }
+}
